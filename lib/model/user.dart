bafb9c04ba68fa6db1aee9c937ec9edb6aa69e46
minas528 <minalem.5d@gmail.com>
2021-07-24 23:52:26 +0300

    fix bug

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,15 +1,54 @@
-class User {
-  final String imagePath;
-  final String name;
-  final String email;
-  final String about;
-  final bool isDarkMode;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-  const User({
-    required this.imagePath,
-    required this.name,
-    required this.email,
-    required this.about,
-    required this.isDarkMode,
-  });
+class User {
+  final String uid;
+  final String name;
+  final String phone;
+  final String password;
+  final String houseNo;
+  final String address;
+  final LatLng latLng;
+  final double rating;
+  final Object comments;
+  final String role;
+
+  User(
+      {required this.uid,
+      required this.name,
+      required this.phone,
+      required this.password,
+      required this.houseNo,
+      required this.address,
+      required this.latLng,
+      required this.rating,
+      required this.comments, 
+      required this.role});
+
+  User.fromJson(Map<String, Object?> json, String reference)
+      : this(
+          uid: reference,
+          name: json['name']! as String,
+          phone: json['phone']! as String,
+          password: json['password']! as String,
+          houseNo: json['houseNo']! as String,
+          address: json['address']! as String,
+          latLng: json['latLng']! as LatLng,
+          comments: json['comments']!,
+          rating: (json['rating']! as num) as double,
+          role: json['role']! as String,
+        );
+
+  Map<String, Object?> toJson() {
+    return {
+      'name': name,
+      'phone': phone,
+      'password': password,
+      'houseNo': houseNo,
+      'address': address,
+      'latLng': latLng,
+      'rating': rating,
+      'comments': comments, 
+      'role': role,
+    };
+  }
 }
